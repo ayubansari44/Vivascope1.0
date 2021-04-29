2bb140806252a3e4f81bd602a92c316398bc2895
Ayub <ayubansari1999@gmail.com>
2021-04-29 11:05:31 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {MediaObserver, MediaChange} from '@angular/flex-layout';
+import { Subscription} from 'rxjs';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit,OnDestroy {
   title = 'Page1';
+  mediaSub: Subscription;
+  deviceMd:boolean;
+  showModal:boolean;
+  constructor(public mediaObserver: MediaObserver){
+
+  }
+
+  ngOnInit(){
+    this.mediaSub=this.mediaObserver.media$.subscribe((result:MediaChange)=>{
+      console.log(result.mqAlias);
+      this.deviceMd=result.mqAlias==='md' ? true:false;
+    })
+  }
+
+  ngOnDestroy(){
+    this.mediaSub.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule} from '@angular/flex-layout';
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import {MatDialogModule} from "@angular/material/dialog";
+import { PatientListComponent } from './patient-list/patient-list.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {ModalModule } from "./_modal";
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator'; 
+import {MatExpansionModule} from '@angular/material/expansion';
+import { MatTabsModule } from '@angular/material/tabs';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, PatientListComponent],
+  entryComponents: [PatientListComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    FormsModule,
+    NgbModule,
+    ModalModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatExpansionModule,
+    MatTabsModule,
   ],
+
   providers: [],
-  bootstrap: [AppComponent]
+
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
